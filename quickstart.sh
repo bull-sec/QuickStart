131b3b76aa767f0af55052199c2e3c5eb744655e
bullsec <bullsec@protonmail.com>
2021-07-20 19:42:07 +0100

    added dot files and setup script

---
+++ quickstart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Set a PROPER shell
+chsh -s /bin/bash
+
+## Make it functional
+cp .bashrc $HOME/.bashrc
+
+## Make it fancy
+cp .inputrc $HOME/.bashrc
+
+## Make it RAIN!
+mkdir $HOME/.tmux
+cp .tmux.conf $HOME/.tmux/.tmux.conf
+
+## Final touches
+cd $HOME
+ln -s .tmux/.tmux.conf
+
+

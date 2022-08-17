4b54a6b7caa9e2147b9a8f50431947fb2829adc1
bullsec <bullsec@protonmail.com>
2022-08-17 11:10:29 +0000

    fixed install script

--- quickstart.sh
+++ quickstart.sh
@@ -7,16 +7,18 @@ chsh -s /bin/bash
 cp .bashrc $HOME/.bashrc
 
 ## Make it fancy
-cp .inputrc $HOME/.bashrc
+cp .inputrc $HOME/.inputrc
 
 ## Make it RAIN!
 mkdir $HOME/.tmux
 cp .tmux.conf $HOME/.tmux/.tmux.conf
 
+# Make VIM great again!
+cp .vimrc $HOME/.vimrc
+
 ## Final touches
 cd $HOME
 ln -s .tmux/.tmux.conf
-cp .vimrc ~/.vimrc
 
 echo -e "\nSystem installed, enjoy your new terminal environment\n"
 

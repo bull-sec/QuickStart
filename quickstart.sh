ee83e1e71437fc0aad6258935e9a16c58df21b16
bullsec <bullsec@protonmail.com>
2022-08-17 10:57:11 +0000

    added vimrc and updated installer script

--- quickstart.sh
+++ quickstart.sh
@@ -16,5 +16,7 @@ cp .tmux.conf $HOME/.tmux/.tmux.conf
 ## Final touches
 cd $HOME
 ln -s .tmux/.tmux.conf
+cp .vimrc ~/.vimrc
 
+echo -e "\nSystem installed, enjoy your new terminal environment\n"
 
